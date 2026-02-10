fe20b3f5ab0097ac388112e06f23abcbbcc2e87e
Shivayan Chakravarty <shivayanchak@gmail.com>
2026-02-11 00:50:04 +0530

    page.tsx issue

--- client_next/app/page.tsx
+++ client_next/app/page.tsx
@@ -4,17 +4,27 @@ import { redirect } from "next/navigation";
 export default async function RootPage() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
+
   if (!token) {
     redirect("/auth/login");
   }
+
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
-  const res = await fetch(BACKEND_URL + "/api/auth/me", { headers: { Cookie: `token=${token}`, }, cache: "no-store", });
+
+  const res = await fetch(`${BACKEND_URL}/api/auth/me`, {
+    credentials: "include",
+    cache: "no-store",
+  });
+
   if (!res.ok) {
     redirect("/auth/login");
   }
+
   const { user } = await res.json();
+
   if (!user.isProfileComplete) {
     redirect("/auth/profile");
   }
+
   redirect("/main");
 }
