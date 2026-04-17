a253763fc3f15b809e6ff19d65c31ee12a60b850
Dhanuja2006 <dhanujavidyat@gmail.com>
2026-04-17 09:01:57 +0530

    fix: updated navbar redirection logic

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import Link from "next/link";
 import NotificationBell from "./NotificationBell";
 
 export default function Navbar() {
   return (
     <div className="flex items-center justify-center md:justify-between py-4 px-6 md:px-10 bg-black/10 backdrop-blur-3xl w-[90vw] md:w-[70vw] mx-auto border border-black/10 dark:border-white/20 rounded-full my-5">
-      <p className="font-extrabold text-[1.1rem] font-serif text-white"> Vector </p>
-      <div className="hidden md:flex gap-20 text-gray-200 items-center cursor-pointer">
-        <p className="transition-all duration-300 hover:text-white"> Home </p>
-        <p className="transition-all duration-300 hover:text-white"> Contact Us </p>
-        <p className="transition-all duration-300 hover:text-white"> Support</p>
-        <NotificationBell/>
+      <p className="font-extrabold text-[1.1rem] font-serif text-white">Vector</p>
+      <div className="hidden md:flex gap-20 text-gray-200 items-center">
+        <Link href="/main" className="transition-all duration-300 hover:text-white">
+          Home
+        </Link>
+        <Link href="#contact" className="transition-all duration-300 hover:text-white">
+          Contact Us
+        </Link>
+        <Link href="#support" className="transition-all duration-300 hover:text-white">
+          Support
+        </Link>
+        <NotificationBell />
       </div>
     </div>
   );
