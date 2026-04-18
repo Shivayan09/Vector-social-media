ecfb9f5db6c66c012f9d0ff6cc5390e6dbc426b6
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-04-18 15:04:01 +0530

    feat(api): Add endpoints for unread message tracking

--- server/src/routes/message.routes.js
+++ server/src/routes/message.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteMessage, getMessages, sendMessage } from "../controllers/message.controller.js";
+import { deleteMessage, getMessages, sendMessage, markConversationAsRead, getUnreadCount } from "../controllers/message.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const messageRouter = express.Router();
 
 messageRouter.get("/:conversationId", authMiddleware, getMessages);
+messageRouter.get("/:conversationId/unread-count", authMiddleware, getUnreadCount);
 messageRouter.post("/", authMiddleware, sendMessage);
+messageRouter.patch("/:conversationId/read-all", authMiddleware, markConversationAsRead);
 messageRouter.delete("/:messageId", authMiddleware, deleteMessage);
 
 export default messageRouter;
