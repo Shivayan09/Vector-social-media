29b38df14f70f077251e456975fe943609b170d6
Avi Singh <singhavi16.official@gmail.com>
2026-04-30 19:32:38 +0530

    fix: resolve lint error and correct route order for suggested users

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,18 +1,31 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
-import { getAllUsers, getFollowers, getFollowing, getUserProfile, searchUsers, toggleFollowUser, updateProfile, uploadAvatar, getSuggestedUsers } from "../controllers/user.controller.js";
+import {
+  getAllUsers,
+  getFollowers,
+  getFollowing,
+  getUserProfile,
+  searchUsers,
+  toggleFollowUser,
+  updateProfile,
+  uploadAvatar,
+  getSuggestedUsers,
+} from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/avatar", authMiddleware, upload.single("avatar"), uploadAvatar);
 userRouter.put("/update-profile", authMiddleware, updateProfile);
 userRouter.put("/:id/follow", authMiddleware, toggleFollowUser);
+
 userRouter.get("/all", getAllUsers);
 userRouter.get("/search", searchUsers);
-userRouter.get("/:username", getUserProfile);
+userRouter.get("/suggested", authMiddleware, getSuggestedUsers); 
+
 userRouter.get("/:id/followers", authMiddleware, getFollowers);
 userRouter.get("/:id/following", authMiddleware, getFollowing);
-userRouter.get("/suggested", authMiddleware, getSuggestedUsers);
+
+userRouter.get("/:username", getUserProfile); 
 
 export default userRouter;
