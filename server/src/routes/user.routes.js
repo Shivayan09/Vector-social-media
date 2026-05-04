3e0d5d91716222b56c2d5110d3dcefcd6fe60320
Diksha Dabhole <dikshadabhole78@gmail.com>
2026-05-04 22:46:13 +0530

    Add optional auth middleware to include follow status in profile

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,26 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
+import User from "../models/user.model.js";
 import { getAllUsers, getFollowers, getFollowing, getUserProfile, searchUsers, toggleFollowUser, updateProfile, uploadAvatar, getSuggestedUsers } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
+
+// Optional auth middleware - sets req.user if valid token exists, otherwise continues
+const optionalAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token;
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.id);
+      if (user) req.user = user;
+    }
+  } catch (error) {
+    // Silently ignore - user is just not authenticated
+  }
+  next();
+};
 
 userRouter.post("/avatar", authMiddleware, upload.single("avatar"), uploadAvatar);
 userRouter.put("/update-profile", authMiddleware, updateProfile);
@@ -11,7 +28,7 @@ userRouter.put("/:id/follow", authMiddleware, toggleFollowUser);
 userRouter.get("/suggestions", authMiddleware, getSuggestedUsers);
 userRouter.get("/all", getAllUsers);
 userRouter.get("/search", searchUsers);
-userRouter.get("/:username", getUserProfile);
+userRouter.get("/:username", optionalAuth, getUserProfile);
 userRouter.get("/:id/followers", authMiddleware, getFollowers);
 userRouter.get("/:id/following", authMiddleware, getFollowing);
 
