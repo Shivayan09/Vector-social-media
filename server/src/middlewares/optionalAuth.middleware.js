ad1d61dc7df6160dcbbd0e815c293b8543044975
Vivek Arya <vivekaryaa369@gmail.com>
2026-05-10 13:26:00 +0530

    Refactor optionalAuthMiddleware to simplify logic
    Removed redundant token verification logic from optionalAuthMiddleware.

--- server/src/middlewares/optionalAuth.middleware.js
+++ server/src/middlewares/optionalAuth.middleware.js
@@ -13,18 +13,6 @@ const optionalAuthMiddleware = async (req, res, next) => {
             req.user = user;
         }
         next();
-    } const optionalAuthMiddleware = async (req, res, next) => {
-    try {
-        const token = req.cookies?.token;
-        if (!token) {
-            return next();
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id);
-        if (user) {
-            req.user = user;
-        }
-        next();
     } catch (error) {
         // If token is invalid, just proceed as guest
         next();
