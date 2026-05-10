3235660d66604640e6cc43f02e779a24c3f7d3aa
Vivek Arya <vivekaryaa369@gmail.com>
2026-05-10 13:40:44 +0530

    Fix unused variable error in optionalAuth middleware
    Removed unused variable 'error' from optionalAuth middleware.

--- server/src/middlewares/optionalAuth.middleware.js
+++ server/src/middlewares/optionalAuth.middleware.js
@@ -14,8 +14,6 @@ const optionalAuthMiddleware = async (req, res, next) => {
         }
         next();
     } catch (error) {
-       /home/runner/work/Vector-social-media/Vector-social-media/server/src/middlewares/optionalAuth.middleware.js
-16:14  error  'error' is defined but never used  @typescript-eslint/no-unused-vars
         next();
     }
 };
