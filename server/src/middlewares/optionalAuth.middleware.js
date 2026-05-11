0c8eee3a9216c479ce598f9ce0ab8ca32be5f617
Vivek Arya <vivekaryaa369@gmail.com>
2026-05-11 18:41:52 +0530

    Update optionalAuth.middleware.js

--- server/src/middlewares/optionalAuth.middleware.js
+++ server/src/middlewares/optionalAuth.middleware.js
@@ -4,16 +4,21 @@ import User from "../models/user.model.js";
 const optionalAuthMiddleware = async (req, res, next) => {
     try {
         const token = req.cookies?.token;
+
         if (!token) {
             return next();
         }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
         const user = await User.findById(decoded.id);
+
         if (user) {
             req.user = user;
         }
+
         next();
-    } catch (error) {
+    } catch {
         next();
     }
 };
