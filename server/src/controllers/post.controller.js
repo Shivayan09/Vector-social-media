a0cd03eea002aece66d5cc4f008eb17bf6202260
Jainiksha patel <jainiksha.patel2007@gmail.com>
2026-06-27 17:34:17 +0530

    Update post.controller.js

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -130,14 +130,25 @@ const getTopPosts = (daysAgo, maxResults) => async (req, res) => {
       { $unwind: "$author" },
       {
         $project: {
-          _id: 1, content: 1, image: 1, intent: 1, likes: 1,
-          commentsCount: 1, sharesCount: 1, likesCount: 1,
-          createdAt: 1, updatedAt: 1, poll: 1,
-          createdAt: 1, updatedAt: 1,
-          isEdited: 1, editedAt: 1,
-          "author._id": 1, "author.username": 1, "author.name": 1,
-          "author.surname": 1, "author.avatar": 1,
-        },
+  _id: 1,
+  content: 1,
+  image: 1,
+  intent: 1,
+  likes: 1,
+  commentsCount: 1,
+  sharesCount: 1,
+  likesCount: 1,
+  createdAt: 1,
+  updatedAt: 1,
+  poll: 1,
+  isEdited: 1,
+  editedAt: 1,
+  "author._id": 1,
+  "author.username": 1,
+  "author.name": 1,
+  "author.surname": 1,
+  "author.avatar": 1,
+},
       }
     ]);
 
